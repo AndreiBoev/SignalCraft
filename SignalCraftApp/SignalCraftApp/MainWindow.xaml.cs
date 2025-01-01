1004cc19f9c603aa3c4eed71a6ada076cafa21bb
Боев Андрей Олегович <aoboev@edu.hse.ru>
2025-01-02 02:54:06 +0300

    add COM functions

--- SignalCraftApp/SignalCraftApp/MainWindow.xaml.cs
+++ SignalCraftApp/SignalCraftApp/MainWindow.xaml.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
+using System.IO.Ports;
+using System.Linq;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 using AForge.Video;
 using AForge.Video.DirectShow;
@@ -16,15 +23,78 @@ namespace SignalCraftApp
     {
         private FilterInfoCollection _videoDevices; // Список доступных камер
         private VideoCaptureDevice _videoSource;    // Устройство видеозахвата
+        private SerialPort _port = new SerialPort(); // Com-порт
+        private long _count; // Число полученных пакетов
+
 
         public MainWindow()
         {
             InitializeComponent();
         }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             // Загружаем доступные камеры
             BtnSearchCameras_Click(null, null);
+
+            // Загрузка com-портов
+            BtnUpdatePorts_Click(null, null);
+            _port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Этот код выполняется в потоке, отличном от UI
+            try
+            {
+                string receivedData = _port.ReadLine();
+                // Обновление UI должно происходить в UI-потоке
+                Dispatcher.Invoke(() =>
+                {
+                    ProcessReceivedData(receivedData);
+                });
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show($"Ошибка при чтении данных из COM-порта: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                });
+
+            }
+
+        }
+
+        private void ProcessReceivedData(string data)
+        {
+            if (data.Length > 1)
+                data = data.Remove(data.Length - 1);
+
+            string dataOut;
+
+            if (CBTimeMark.IsChecked.Value)
+                dataOut = DateTime.Now.ToString("HH:mm:ss") + " -> " + data;
+            else
+                dataOut = data;
+
+            _count++;
+            TBlockCount.Text = _count.ToString();
+
+            try
+            {
+                Packet packet = new Packet(data);
+                
+                dataOut += " -> " + "OK" + '\n';
+            }
+            catch (Exception ex)
+            {
+                dataOut += " -> " + ex.Message + '\n';
+            }
+
+            TBData.Text += dataOut;
+            if (CBAutoScroll.IsChecked.Value)
+                TBData.ScrollToEnd();
         }
 
         private void BtnSearchCameras_Click(object sender, RoutedEventArgs e)
@@ -65,11 +135,14 @@ namespace SignalCraftApp
             // Конвертируем полученный кадр в BitmapImage для отображения в WPF
             Dispatcher.Invoke(() =>
             {
+
                 var bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
-                bitmap.GetHbitmap(),
-                IntPtr.Zero,
-                System.Windows.Int32Rect.Empty,
-                BitmapSizeOptions.FromEmptyOptions());
+                    bitmap.GetHbitmap(),
+                    IntPtr.Zero,
+                    System.Windows.Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+
+                bitmap.Dispose();
 
                 // Отображаем видео в Image
                 ImCamera.Source = bitmapSource;
@@ -147,12 +220,91 @@ namespace SignalCraftApp
                 }
             }
         }
-       
+
         private void Window_Closed(object sender, EventArgs e)
         {
             // Выключаем камеру, если она не выключена
             if (_videoSource != null)
                 BtnCamStartAndStop_Click(null, null);
+        }
+
+        private void BtnUpdatePorts_Click(object sender, RoutedEventArgs e)
+        {
+            CBPorts.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length == 0)
+            {
+                CBPorts.Items.Add("Не найдено");
+                CBPorts.SelectedIndex = 0;
+                BtnAction.IsEnabled = false;
+                return;
+            }
+            foreach (string port in ports)
+            {
+                CBPorts.Items.Add(port);
+            }
+            CBPorts.SelectedIndex = ports.Length - 1;
+            BtnAction.IsEnabled = true;
+        }
+
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (_port.IsOpen)
+            {
+                _port.Write(TBSend.Text);
+                TBData.Text += TBSend.Text + '\n';
+                TBSend.Clear();
+            }
+        }
+
+        private void TBSend_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                BtnSend_Click(null, null);
+        }
+
+        private void BtnAction_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_port.IsOpen)
+            {
+                try
+                {
+                    _port.PortName = CBPorts.Text;
+                    _port.BaudRate = int.Parse(CBSpeed.Text);
+                    _port.Open();
+                    (BtnAction.Content as System.Windows.Controls.Image).Source = new BitmapImage(new Uri(@"/Resources/Icons/Stop.png", UriKind.RelativeOrAbsolute));
+                    BtnSend.IsEnabled = true;
+                    TBSend.IsEnabled = true;
+                    CBSpeed.IsEnabled = false;
+                    CBPorts.IsEnabled = false;
+                    BtnUpdatePorts.IsEnabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    BtnUpdatePorts_Click(null, null);
+                }
+            }
+            else
+            {
+                _port.Close();
+                (BtnAction.Content as System.Windows.Controls.Image).Source = new BitmapImage(new Uri(@"/Resources/Icons/Start.png", UriKind.RelativeOrAbsolute));
+                TBData.Clear();
+                BtnSend.IsEnabled = false;
+                TBSend.IsEnabled = false;
+                CBSpeed.IsEnabled = true;
+                CBPorts.IsEnabled = true;
+                BtnUpdatePorts.IsEnabled = true;
+                TBlockCount.Text = "0";
+                _count = 0;
+            }
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            _count = 0;
+            TBlockCount.Text = "0";
+            TBData.Clear();
         }
     }
 }
---
+++ SignalCraftApp/SignalCraftApp/Packet.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Windows.Markup;
+
+public class Packet
+{
+    public byte StartByte { get; private set; } = 0xAA;
+    public byte Length { get; private set; }
+    public byte SignalType { get; private set; }
+    public byte Direction { get; private set; }
+    public byte BusNumber { get; private set; }
+    public byte[] Data { get; private set; }
+    public ushort CRC { get; private set; }
+    public byte StopByte { get; private set; } = 0xFF;
+
+    public Packet(byte signalType, byte direction, byte busNumber, byte[] data)
+    {
+        SignalType = signalType;
+        Direction = direction;
+        BusNumber = busNumber;
+        Data = data;
+        Length = (byte)(3 + Data.Length); // Тип сигнала + Направление + Номер шины + Данные.
+        CRC = CalculateCRC();
+    }
+
+    public Packet(string packetString)
+    {
+        byte[] bytes = ParseHexString(packetString);
+
+        if (bytes.Length < 6 || bytes[0] != 0xAA || bytes[bytes.Length - 1] != 0xFF)
+            throw new ArgumentException("Invalid packet format");
+
+        StartByte = bytes[0];
+        Length = bytes[1];
+        SignalType = bytes[2];
+        Direction = (byte)(bytes[3] & 0x01); // Направление - 1 бит
+        BusNumber = bytes[4];
+
+        int dataLength = Length - 3;
+        Data = new byte[dataLength];
+        Array.Copy(bytes, 5, Data, 0, dataLength);
+
+        CRC = BitConverter.ToUInt16(bytes, bytes.Length - 3);
+        StopByte = bytes[bytes.Length - 1];
+
+        if (CRC != CalculateCRC())
+            throw new ArgumentException("CRC validation failed");
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendFormat("{0:X2} {1:X2} {2:X2} {3:X2} {4:X2} ", StartByte, Length, SignalType, Direction, BusNumber);
+        foreach (var b in Data)
+            sb.AppendFormat("{0:X2} ", b);
+        sb.AppendFormat("{0:X2} {1:X2} {2:X2}", (CRC >> 8) & 0xFF, CRC & 0xFF, StopByte);
+        return sb.ToString();
+    }
+
+    private ushort CalculateCRC()
+    {
+        ushort crc = 0xFFFF;  // Инициализация CRC значением 0xFFFF
+        ushort polynomial = 0x8005; // Полином для CRC-16-ANSI
+
+        // Include the length byte, signal type, direction, and bus number in CRC calculation.
+        crc = CalculateByteCRC(crc, polynomial, Length);
+        crc = CalculateByteCRC(crc, polynomial, SignalType);
+        crc = CalculateByteCRC(crc, polynomial, (byte)((Direction << 7) | BusNumber)); // Combine Direction and bus number
+
+        // Include the data bytes in the CRC calculation.
+        foreach (byte b in Data)
+        {
+            crc = CalculateByteCRC(crc, polynomial, b);
+        }
+
+        return crc;
+    }
+
+    private ushort CalculateByteCRC(ushort crc, ushort polynomial, byte dataByte)
+    {
+        crc ^= (ushort)(dataByte);
+
+        for (int i = 0; i < 8; i++)
+        {
+            if ((crc & 0x0001) != 0)  // Check the least significant bit.
+                crc = (ushort)((crc >> 1) ^ polynomial); // Right shift and XOR with polynomial
+            else
+                crc >>= 1;  // Right Shift
+        }
+        return crc;
+    }
+
+
+    private byte[] ParseHexString(string hexString)
+    {
+        string[] hexValues = hexString.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+        byte[] bytes = new byte[hexValues.Length];
+        for (int i = 0; i < hexValues.Length; i++)
+            bytes[i] = Convert.ToByte(hexValues[i], 16);
+        return bytes;
+    }
+}
